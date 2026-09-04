c54b98234fb49eca674f8ae0cc298b1262207a82
NamaKamu <emailkamu@gmail.com>
2026-09-05 07:56:35 +0900

    Initial commit for new game

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: Scaffold(
+      body: Center(
+        child: Text('Game Baru Siap Dibuat!'),
+      ),
+    ),
+  ));
+}
